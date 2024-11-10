5b75a9d3c1234a1f5ae4394e7ed4e5188ec7bbb3
Tom Montgomery <montgomery3681@gmail.com>
2024-11-10 14:02:36 -0700

    Initial Commit

---
+++ Fancy/FancyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FancyApp.swift
+//  Fancy
+//
+//  Created by tom montgomery on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FancyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
